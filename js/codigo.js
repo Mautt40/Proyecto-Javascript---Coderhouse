e7aa67de4b0e892b67d3f2ca707866afe250edd5
Mautt <tondato.t100@gmail.com>
2023-03-01 04:15:16 -0300

    falta local storage

--- js/codigo.js
+++ js/codigo.js
@@ -54,91 +54,137 @@ let bienvenida = document.createElement ("p")
 bienvenida.innerText = "Bienvenido al simulador de puntos Dynamo"  
 bienvenida.style.fontSize = "45px"; 
 
+
 cabecera.append(bienvenida);
 
 
 //funcionalidad card Super//
 
+let array = [];
+
+
+
 let boton_super = document.getElementById ("litros_super");
 boton_super.addEventListener("click",function() {
-servicios = "super";
+combustible = "Super"    
+
+
 });
 
 let litros_super = document.getElementById ("litros_super");
 litros_super.addEventListener ("change", function(e){ 
 
-console.log(e.target.value);
-
-if (servicios === "super"){ 
 precio = parseInt(e.target.value) * 125;
-puntos = parseInt(e.target.value) + 50;
+puntos = parseInt(e.target.value) + 100;
 
-console.log("precio " + precio)
-console.log("puntos " + puntos)
 
+let resultado = {
+    combustible: combustible,
+    Total_litros: (e.target.value),
+    precio: precio,
+    puntos: puntos,
 }  
 
-});
-let btn_calcular_super = document.getElementById ("calc_super");
-btn_calcular_super.addEventListener("click", function(e){
+array.push (resultado);
+console.log (array);
 
-console.log ("cálculo super"); 
+mostrar_tabla();
 
-});
+},
+)
+ //acá iría un local storage//
 
 //funcionalidad card premium (que es igual a la otra pero con otros Id, rompiendo el paradigama KISS y el DRY, PERO NO ME SALIÓ DE OTRA MANERA y estoy aprendiendo :(
 
 let boton_premium = document.getElementById ("litros_premium");
 boton_premium.addEventListener("click", function() {  
-servicios = "premium";
+combustible = "premium";
 
 });
 
 let litros_premium = document.getElementById ("litros_premium");
 litros_premium.addEventListener ("change", function(e){ 
 
-console.log(e.target.value);
-
-if (servicios === "premium"){ 
 precio = parseInt(e.target.value) * 150;
-puntos = parseInt(e.target.value) + 70;
+puntos = parseInt(e.target.value) + 150;
+  
+let resultado_premium = {
+    
+    combustible: combustible,
+    Total_litros: (e.target.value),
+    precio: precio,
+    puntos: puntos,
+}  
 
-console.log("precio " + precio)
-console.log("puntos " + puntos)
-}
-});  
+array.push (resultado_premium);
+console.log (array);
 
-let btn_calcular_premium = document.getElementById ("cal_pre");
-btn_calcular_premium.addEventListener("click", function(e){
+mostrar_tabla();
 
-console.log("cálculo premium");
 
-}); 
+})
 
-// let = KISS DRY *3 - Card 3//
+// KISS & DRY *3 - Card 3//
 
 let boton_bio = document.getElementById ("litros_bio");
 boton_bio.addEventListener("click",function() {
-servicios = "Biodiesel";
+combustible = "biodiesel";
 });
 
 let litros_bio = document.getElementById ("litros_bio");
 litros_bio.addEventListener ("change", function(e){ 
 
-console.log(e.target.value);
-
-if (servicios === "biodiesel"){ 
 precio = parseInt(e.target.value) * 180;
-puntos = parseInt(e.target.value) + 90;
+puntos = parseInt(e.target.value) + 200;
 
-console.log ("precio " + precio);
-console.log ("puntos " + puntos);
-} 
+let resultado_bio = {
+    combustible: combustible,
+    Total_litros: (e.target.value),
+    precio: precio,
+    puntos: puntos,
+}  
+
+array.push (resultado_bio);
+console.log (array);
+
+mostrar_tabla();
 });
 
-let btn_calcular_bio = document.getElementById ("calc_bio");
-btn_calcular_bio.addEventListener("click", function(e){
+function mostrar_tabla (){
+    
+    let tabla = document.getElementById ("tbody");
+    tabla.innerHTML = "";
 
-console.log("cálculo biodiesel");
+    for (let resultado of array){
 
-});
+        let fila = document.createElement ("tr");
+        fila.innerHTML = 
+        `<td> ${resultado.combustible} </td>
+         <td> ${resultado.Total_litros} </td>
+         <td> ${resultado.precio} </td>
+         <td> ${resultado.puntos} </td>
+         <td> <button class= "btn btn-warning borrar_elemento "> Borrar </button> </td>`
+
+
+         tabla.append(fila);
+
+    }     
+
+let btn_borrar = document.querySelectorAll (".borrar_elemento");
+
+for (let btn of btn_borrar){
+    btn.addEventListener ("click", borrar_elemento);
+}
+
+}
+
+function borrar_elemento (e){
+
+let abuelo = e.target.parentNode.parentNode;
+
+abuelo.remove ()
+
+console.log (array);
+
+
+}
